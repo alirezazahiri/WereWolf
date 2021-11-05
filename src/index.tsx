a787120693a7ec083581fbc7530a58c111c4ca83
Alireza <itsalireza.zr@gmail.com>
2021-11-05 12:58:08 +0330

    initial Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// Components 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Style
+import "./index.css";
+
+// dists
+import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
