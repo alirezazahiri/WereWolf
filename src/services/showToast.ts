0c14401fa383df665dfc00b3d4bde12164b09cd8
Alireza <itsalireza.zr@gmail.com>
2021-11-06 22:50:07 +0330

    toast backdrop removed

--- src/services/showToast.ts
+++ src/services/showToast.ts
@@ -1,8 +1,7 @@
 import toast from "react-hot-toast";
 
 const SUCCESS_STYLE = {
-    background: "rgba(0, 0, 0, .3)",
-    backdropFilter: "blur(20px)",
+    background: "rgb(0, 0, 0)",
     border: "1px solid #50cb93",
     boxShadow: "0 0 12px #50cb93",
     padding: "1rem",
@@ -11,8 +10,7 @@ const SUCCESS_STYLE = {
 };
 
 const ERROR_STYLE = {
-    background: "rgba(0, 0, 0, .3)",
-    backdropFilter: "blur(20px)",
+    background: "rgb(0, 0, 0)",
     border: "1px solid #DA0037",
     boxShadow: "0 0 12px #DA0037",
     padding: "1rem",
