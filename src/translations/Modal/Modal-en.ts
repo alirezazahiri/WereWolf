0f7bbcc8fc78fcdcd04a060d1f69c44dc29466a4
Alireza Zahiri <itsalireza.zr@gmail.com>
2022-03-01 14:08:27 +0330

    submit translation added!

--- src/translations/Modal/Modal-en.ts
+++ src/translations/Modal/Modal-en.ts
@@ -1,10 +1,11 @@
 const buttons = {
-    close: "close",
-    back_to_name_enter: "Back",
-    start: "Start Game",
-    go_to_char_select: "Go to Character Select",
-    see_characters: "Characters",
-    accept_and_continue: "Accept & Continue"
-  };
+  close: "close",
+  back_to_name_enter: "Back",
+  start: "Start Game",
+  go_to_char_select: "Go to Character Select",
+  see_characters: "Characters",
+  accept_and_continue: "Accept & Continue",
+  submit: "Confirm",
+};
 
-export const modal_objects_en = {buttons}
+export const modal_objects_en = { buttons };
